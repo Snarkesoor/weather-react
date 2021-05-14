0c47e806abb30b33dbc93c230c4a63e22f320e29
Snarkesoor <78655276+Snarkesoor@users.noreply.github.com>
2021-05-14 21:47:53 +0200

    Added search engine and current position functions

--- src/Today.js
+++ src/Today.js
@@ -2,11 +2,6 @@ import React from "react";
 import "./Today.css";
 
 export default function Today(props) {
-  let weatherToday = {
-     minimum: 5,
-    humidity: 80,
-    windSpeed: 5
-  };
   return (
     <div className="today">
       <p className="date-today">Today's weather:<sup className="converter">
@@ -22,15 +17,14 @@ export default function Today(props) {
       <p className="temp-today">
         <img src="" id="icon-1" alt="" width="50px"></img>
         <br />
-        <span className="temp">{props.data.temperature}</span>° |{" "}
-        <span className="min">{weatherToday.minimum}</span>°
+        <span className="temp">{props.data.temperature}</span>° 
       </p>
       <ul className="weather-details">
         <li>
-          Humidity: <span id="humidity">{weatherToday.humidity}</span>%
+          Humidity: <span id="humidity">{props.data.humidity}</span>%
         </li>
         <li>
-          Wind speed: <span id="wind">{weatherToday.windSpeed}</span>km/h
+          Wind speed: <span id="wind">{props.data.wind}</span>km/h
         </li>
       </ul>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,7 @@ import CurrentDate from "./CurrentDate";
 
 export default function Weather(props) {
   let [weatherData, setWeatherData] = useState({ ready: false });
+let [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     console.log(response.data)
@@ -15,9 +16,39 @@ export default function Weather(props) {
       temperature: Math.round(response.data.main.temp),
       city: response.data.name,
       humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
+      wind: Math.round(response.data.wind.speed * 3.6),
       date: new Date(response.data.dt * 1000)
     });
+  }
+
+
+  function retrievePosition(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(handleResponse);
+}
+
+  function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+  function search() {
+  let apiKey = "6c12bf653a5233b9ac28d0707b11b7e6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
   
   if (weatherData.ready) {
@@ -28,11 +59,10 @@ export default function Weather(props) {
       <h1>
         <span id="h1city">{weatherData.city}</span>
               </h1>
-
     </div>
 
       <div className="col-3">
-      <form className="d-flex">
+      <form className="d-flex" onSubmit={handleSubmit}>
         <input
           className="form-control me-2"
           id="search-city-form"
@@ -40,11 +70,12 @@ export default function Weather(props) {
           placeholder="Search city"
           aria-label="Search"
           autoFocus="on"
+          onChange={handleCityChange}
         />
       </form>
     </div>
         <div className="col-3">
-      <button>Current location</button>
+      <button onClick={getCurrentPosition}>Current location</button>
     </div>
       </div> 
   <h3><CurrentDate date={weatherData.date} /> </h3>
@@ -54,10 +85,7 @@ export default function Weather(props) {
   </div>
   ); 
 } else {   
-  let apiKey = "6c12bf653a5233b9ac28d0707b11b7e6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
+  search();
 
   return "Loading..."
 }
